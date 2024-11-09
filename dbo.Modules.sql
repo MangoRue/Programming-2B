df88eaf146a8da780367f8a8607624e5900d68ee
Rutendo <166141557+MangoRue@users.noreply.github.com>
2024-11-09 11:06:18 +0200

    Documentation and database for the code

---
+++ dbo.Modules.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Modules] (
+    [Id]                    INT            IDENTITY (1, 1) NOT NULL,
+    [Code]                  NVARCHAR (MAX) NOT NULL,
+    [Name]                  NVARCHAR (MAX) NOT NULL,
+    [Credits]               INT            NOT NULL,
+    [ClassHours]            INT            NOT NULL,
+    [SelfStudyHoursPerWeek] INT            NOT NULL,
+    [SemWeeks]              INT            NOT NULL,
+    [StartDate]		        DATETIME2 (7)  NOT NULL,
+	[PlannedDay]			DATETIME2(7)  NOT NULL,
+    CONSTRAINT [PK_Modules] PRIMARY KEY CLUSTERED ([Id] ASC),
+	
+);
+
